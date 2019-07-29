5d58c3b37b7ddd67132119c8a91feeab29011f74
yunlzheng <yunl.zheng@gmail.com>
2019-07-29 22:57:06 +0800

    update version

--- cmd/ktctl/main.go
+++ cmd/ktctl/main.go
@@ -47,7 +47,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "KT Connect"
 	app.Usage = ""
-	app.Version = "0.0.4"
+	app.Version = "0.0.5"
 	app.Authors = []cli.Author{
 		cli.Author{
 			Name: "rdc incubator",
