0f2ca8bf95afb2b397403ec7a075324cd54bd980
yunlzheng <yunl.zheng@gmail.com>
2019-09-11 16:48:21 +0800

    add shadow release sh and update ci script

---
+++ release.sh
@@ -0,0 +1,2 @@
+docker build -t registry.cn-hangzhou.aliyuncs.com/rdc-incubator/kt-connect-shadow -f docker/proxy/Dockerfile .
+docker push registry.cn-hangzhou.aliyuncs.com/rdc-incubator/kt-connect-shadow
