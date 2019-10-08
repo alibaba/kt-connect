9a862bffd16d48b5bb89eb43c6bc34a5040889ad
yunlzheng <yunl.zheng@gmail.com>
2019-10-08 13:27:38 +0800

    #23 fixed ssh private key genertor path issue

--- pkg/kt/connect/connect.go
+++ pkg/kt/connect/connect.go
@@ -74,10 +74,10 @@ func (c *Connect) GetClientSet() (clientset *kubernetes.Clientset, err error) {
 	return
 }
 
-// PrepareSSHPrivateKey
+// PrepareSSHPrivateKey generator ssh private key
 func (connect *Connect) PrepareSSHPrivateKey() (err error) {
-	// TODO: should adaptor windows
-	err = ioutil.WriteFile("/tmp/kt_id_rsa", pk, 400)
+	privateKey := util.PrivateKeyPath()
+	err = ioutil.WriteFile(privateKey, pk, 400)
 	if err != nil {
 		log.Printf("Fails create temp ssh private key")
 	}
