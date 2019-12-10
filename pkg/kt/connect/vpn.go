b66a214a0d50c63aeb6af4a886475a7584daf9ee
yunlzheng <yunl.zheng@gmail.com>
2019-12-10 14:47:29 +0800

    fixed jvm properties for socks proxy

--- pkg/kt/connect/vpn.go
+++ pkg/kt/connect/vpn.go
@@ -35,7 +35,7 @@ func (c *Connect) StartConnect(name string, podIP string, cidrs []string) (err e
 		log.Info().Msgf("==============================================================")
 		log.Info().Msgf("Start SOCKS5 Proxy: export http_proxy=socks5://127.0.0.1:%d", c.ProxyPort)
 		log.Info().Msgf("==============================================================")
-		_ = ioutil.WriteFile(".jvmrc", []byte(fmt.Sprintf("-Dhttp.proxyHost=127.0.0.1\n-Dhttp.proxyPort=%d", c.ProxyPort)), 0644)
+		_ = ioutil.WriteFile(".jvmrc", []byte(fmt.Sprintf("-DsocksProxyHost=127.0.0.1\n-DsocksProxyPort=%d", c.ProxyPort)), 0644)
 		err = util.BackgroundRun(util.SSHDynamicPortForward("127.0.0.1", c.Port, c.ProxyPort), "vpn(ssh)", c.Debug)
 	} else {
 		err = util.BackgroundRun(util.SSHUttle("127.0.0.1", c.Port, podIP, c.DisableDNS, cidrs, c.Debug), "vpn(sshuttle)", c.Debug)
