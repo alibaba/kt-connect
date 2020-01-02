d9b650f816dfd23ac7ec14c17a726a05dcb4b62e
金戟 <jinji.lf@alibaba-inc.com>
2020-01-02 23:36:40 +0800

    always check for cluster domain postfix

--- pkg/proxy/dnsserver/dnsserver.go
+++ pkg/proxy/dnsserver/dnsserver.go
@@ -3,7 +3,6 @@ package dnsserver
 import (
 	"errors"
 	"net"
-	"os"
 	"strconv"
 	"strings"
 
@@ -12,29 +11,37 @@ import (
 )
 
 // dns server
-type server struct{}
+type server struct {
+	config *dns.ClientConfig
+}
+
+// constants
+const resolvFile = "/etc/resolv.conf"
 
 // NewDNSServerDefault create default dns server
 func NewDNSServerDefault() (srv *dns.Server) {
 	srv = &dns.Server{Addr: ":" + strconv.Itoa(53), Net: "udp"}
-	srv.Handler = &server{}
+	config, _ := dns.ClientConfigFromFile(resolvFile)
 
-	config, _ := dns.ClientConfigFromFile("/etc/resolv.conf")
+	srv.Handler = &server{config}
 
-	log.Info().Msgf("Successful load local /etc/resolv.conf")
+	log.Info().Msgf("Successful load local " + resolvFile)
 	for _, server := range config.Servers {
 		log.Info().Msgf("Success load nameserver %s\n", server)
+	}
+	for _, domain := range config.Search {
+		log.Info().Msgf("Success load search %s\n", domain)
 	}
 	return
 }
 
 //ServeDNS query DNS rescord
-func (h *server) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
+func (s *server) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
 	msg := dns.Msg{}
 	msg.SetReply(req)
 	msg.Authoritative = true
 	// Stuff must be in the answer section
-	for _, a := range query(req) {
+	for _, a := range s.query(req) {
 		log.Info().Msgf("%v\n", a)
 		msg.Answer = append(msg.Answer, a)
 	}
@@ -42,60 +49,63 @@ func (h *server) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
 	_ = w.WriteMsg(&msg)
 }
 
-func getDomain(origin string) string {
+func (s *server) getDomain(origin string, stripPostfix bool) string {
 	domain := origin
+	postfix := s.config.Search[0]
 
-	namespace, find := os.LookupEnv("PROXY_NAMESPACE")
-	if !find {
-		namespace = "default"
+	// should not happens, just in case
+	if !strings.Contains(domain, ".") {
+		domain = domain + "."
+	}
+
+	// strip domain postfix, if required
+	dotIndex := strings.Index(domain, ".") + 1
+	if stripPostfix {
+		domain = domain[:dotIndex]
 	}
 
 	// has only one dot at the end of queried domain name
-	if strings.Index(domain, ".") == (len(domain) - 1) {
-		domain = domain + namespace + ".svc.cluster.local."
-		log.Info().Msgf("*** Use in cluster dns address %s\n", domain)
+	if dotIndex == len(domain) {
+		domain = domain + postfix + "."
+		log.Info().Msgf("Format domain %s to %s\n", origin, domain)
 	}
 
-	log.Info().Msgf("Format domain %s to %s\n", origin, domain)
 	return domain
 }
 
-func query(req *dns.Msg) (rr []dns.RR) {
+func (s *server) query(req *dns.Msg) (rr []dns.RR) {
 	if len(req.Question) <= 0 {
 		log.Error().Msgf("*** error: dns Msg question length is 0")
 		return
 	}
 
-	msg := new(dns.Msg)
-	msg.RecursionDesired = true
-
 	qtype := req.Question[0].Qtype
 	name := req.Question[0].Name
-	domain := getDomain(name)
 
-	log.Info().Msgf("Received DNS query for %s: \n", domain)
-	msg.SetQuestion(domain, qtype)
-	rr = exchange(domain, qtype, msg, name)
+	rr, err := s.exchange(s.getDomain(name, false), qtype, name)
+	if IsDomainNotExist(err) {
+		log.Info().Msgf("Retry with domain postfix stripped")
+		rr, _ = s.exchange(s.getDomain(name, true), qtype, name)
+	}
 	return
 }
 
-func getResolvServer() (address string, err error) {
-	config, _ := dns.ClientConfigFromFile("/etc/resolv.conf")
-
-	if len(config.Servers) <= 0 {
+func (s *server) getResolvServer() (address string, err error) {
+	if len(s.config.Servers) <= 0 {
 		err = errors.New("*** error: dns server is 0")
 		return
 	}
 
-	server := config.Servers[0]
-	port := config.Port
+	server := s.config.Servers[0]
+	port := s.config.Port
 
 	address = net.JoinHostPort(server, port)
 	return
 }
 
-func exchange(domain string, Qtype uint16, m *dns.Msg, name string) (rr []dns.RR) {
-	address, err := getResolvServer()
+func (s *server) exchange(domain string, qtype uint16, name string) (rr []dns.RR, err error) {
+	log.Info().Msgf("Received DNS query for %s: \n", domain)
+	address, err := s.getResolvServer()
 	if err != nil {
 		log.Error().Msgf(err.Error())
 		return
@@ -103,7 +113,10 @@ func exchange(domain string, Qtype uint16, m *dns.Msg, name string) (rr []dns.RR
 	log.Info().Msgf("Exchange message for domain %s to dns server %s\n", domain, address)
 
 	c := new(dns.Client)
-	res, _, err := c.Exchange(m, address)
+	msg := new(dns.Msg)
+	msg.RecursionDesired = true
+	msg.SetQuestion(domain, qtype)
+	res, _, err := c.Exchange(msg, address)
 
 	if res == nil {
 		if err != nil {
@@ -114,14 +127,17 @@ func exchange(domain string, Qtype uint16, m *dns.Msg, name string) (rr []dns.RR
 		return
 	}
 
-	if res.Rcode != dns.RcodeSuccess {
-		log.Error().Msgf(" *** invalid answer name %s after %d query for %s\n", name, Qtype, domain)
+	if res.Rcode == dns.RcodeNameError {
+		err = DomainNotExistError{domain}
+		return
+	} else if res.Rcode != dns.RcodeSuccess {
+		log.Error().Msgf(" *** failed to answer name %s after %d query for %s\n", name, qtype, domain)
 		return
 	}
 
 	for _, item := range res.Answer {
 		log.Info().Msgf("response: %s", item.String())
-		r, err := getAnswer(name, domain, item)
+		r, err := s.getAnswer(name, domain, item)
 		if err != nil {
 			return
 		}
@@ -131,7 +147,7 @@ func exchange(domain string, Qtype uint16, m *dns.Msg, name string) (rr []dns.RR
 	return
 }
 
-func getAnswer(name string, inClusterName string, acutal dns.RR) (tmp dns.RR, err error) {
+func (s *server) getAnswer(name string, inClusterName string, acutal dns.RR) (tmp dns.RR, err error) {
 	if name != inClusterName {
 		log.Info().Msgf("origin %s query name is not same %s", inClusterName, name)
 		log.Info().Msgf("origin answer rr to %s", acutal.String())
