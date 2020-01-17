76d04d364a8446d755fe5c8299a77ec06b250f91
yunlzheng <yunl.zheng@gmail.com>
2020-01-17 18:23:57 +0800

    update version to 0.0.10

--- cmd/ktctl/main.go
+++ cmd/ktctl/main.go
@@ -24,7 +24,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "KT Connect"
 	app.Usage = ""
-	app.Version = "0.0.9"
+	app.Version = "0.0.10"
 	app.Authors = command.NewCliAuthor()
 	app.Flags = command.AppFlags(options)
 	app.Commands = command.NewCommands(options)
