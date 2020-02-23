655ad4d71898ed0ae0108714e1aaf44541419874
yunlzheng <yunl.zheng@gmail.com>
2020-02-23 10:05:34 +0800

    fixed ut

--- pkg/proxy/dnsserver/dnsserver_test.go
+++ pkg/proxy/dnsserver/dnsserver_test.go
@@ -2,12 +2,14 @@ package dnsserver
 
 import (
 	"testing"
+
 	"github.com/miekg/dns"
 )
 
 func TestAnswerRewrite(t *testing.T) {
+	s := &server{}
 	acutal, _ := dns.NewRR("tomcat.default.svc.cluster.local. 5 IN A 172.21.4.129")
-	r, err := getAnswer("tomcat.", "tomcat.default.svc.cluster.local.", acutal)
+	r, err := s.getAnswer("tomcat.", "tomcat.default.svc.cluster.local.", acutal)
 	if err != nil {
 		t.Errorf("error")
 		return
