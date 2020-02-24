c386e4ba3b76617be12fabd68272db0f57bf697d
yunlzheng <yunl.zheng@gmail.com>
2020-02-24 09:57:09 +0800

    refator istio api with istio-client go

--- pkg/apiserver/controllers/istio.go
+++ pkg/apiserver/controllers/istio.go
@@ -1,11 +1,12 @@
 package controllers
 
 import (
+	"log"
+
 	"github.com/alibaba/kt-connect/pkg/apiserver/common"
 	"github.com/gin-gonic/gin"
+	versionedclient "istio.io/client-go/pkg/clientset/versioned"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
 )
 
 type IstioController struct {
@@ -14,82 +15,67 @@ type IstioController struct {
 
 func (c *IstioController) VirtualServices(context *gin.Context) {
 	namespace := context.Param("namespace")
-	dynamicClient, err := dynamic.NewForConfig(c.Context.Cluster.Config)
+
+	ic, err := versionedclient.NewForConfig(c.Context.Cluster.Config)
+	if err != nil {
+		log.Fatalf("Failed to create istio client: %s", err)
+	}
+
+	vsList, err := ic.NetworkingV1alpha3().VirtualServices(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to get VirtualService in %s namespace: %s", namespace, err)
+	}
 
 	if err != nil {
 		context.JSON(500, gin.H{
-			"message": "fail init dynamicClient",
+			"message": "fail init istioClient",
 		})
 		return
 	}
 
-	virtualServiceGVR := schema.GroupVersionResource{
-		Group:    "networking.istio.io",
-		Version:  "v1alpha3",
-		Resource: "virtualservices",
-	}
-
-	virtualServices, err := dynamicClient.Resource(virtualServiceGVR).Namespace(namespace).List(metav1.ListOptions{})
-	if err != nil {
-		context.JSON(500, gin.H{
-			"message": "fail get pod log",
-		})
-		return
-	}
-
-	context.JSON(200, virtualServices)
+	context.JSON(200, vsList)
 }
 
 func (c *IstioController) VirtualService(context *gin.Context) {
 	namespace := context.Param("namespace")
 	name := context.Param("name")
-	dynamicClient, err := dynamic.NewForConfig(c.Context.Cluster.Config)
+
+	ic, err := versionedclient.NewForConfig(c.Context.Cluster.Config)
 
 	if err != nil {
 		context.JSON(500, gin.H{
-			"message": "fail init dynamicClient",
+			"message": "fail init istioClient",
 		})
 		return
 	}
 
-	virtualServiceGVR := schema.GroupVersionResource{
-		Group:    "networking.istio.io",
-		Version:  "v1alpha3",
-		Resource: "virtualservices",
-	}
+	vs, err := ic.NetworkingV1alpha3().VirtualServices(namespace).Get(name, metav1.GetOptions{})
 
-	virtualService, err := dynamicClient.Resource(virtualServiceGVR).Namespace(namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		context.JSON(500, gin.H{
-			"message": "fail get pod log",
+			"message": "fail get virtual service",
 		})
 		return
 	}
 
-	context.JSON(200, virtualService)
+	context.JSON(200, vs)
 }
 
 func (c *IstioController) DestinationRules(context *gin.Context) {
 	namespace := context.Param("namespace")
-	dynamicClient, err := dynamic.NewForConfig(c.Context.Cluster.Config)
+	ic, err := versionedclient.NewForConfig(c.Context.Cluster.Config)
 
 	if err != nil {
 		context.JSON(500, gin.H{
-			"message": "fail init dynamicClient",
+			"message": "fail init istioClient",
 		})
 		return
 	}
 
-	destinationruleGVR := schema.GroupVersionResource{
-		Group:    "networking.istio.io",
-		Version:  "v1alpha3",
-		Resource: "destinationrules",
-	}
-
-	destinationrules, err := dynamicClient.Resource(destinationruleGVR).Namespace(namespace).List(metav1.ListOptions{})
+	destinationrules, err := ic.NetworkingV1alpha3().DestinationRules(namespace).List(metav1.ListOptions{})
 	if err != nil {
 		context.JSON(500, gin.H{
-			"message": "fail get pod log",
+			"message": "fail get destinationrules",
 		})
 		return
 	}
@@ -100,18 +86,19 @@ func (c *IstioController) DestinationRules(context *gin.Context) {
 func (c *IstioController) DestinationRule(context *gin.Context) {
 	namespace := context.Param("namespace")
 	name := context.Param("name")
-	dynamicClient, _ := dynamic.NewForConfig(c.Context.Cluster.Config)
+	ic, err := versionedclient.NewForConfig(c.Context.Cluster.Config)
 
-	destinationruleGVR := schema.GroupVersionResource{
-		Group:    "networking.istio.io",
-		Version:  "v1alpha3",
-		Resource: "destinationrules",
-	}
-
-	destinationrule, err := dynamicClient.Resource(destinationruleGVR).Namespace(namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		context.JSON(500, gin.H{
-			"message": "fail get pod log",
+			"message": "fail init istioClient",
+		})
+		return
+	}
+
+	destinationrule, err := ic.NetworkingV1alpha3().DestinationRules(namespace).Get(name, metav1.GetOptions{})
+	if err != nil {
+		context.JSON(500, gin.H{
+			"message": "fail get destinationrule",
 		})
 		return
 	}
