17c7cca3d966a539a34cf17d7d5b1163c14d1bed
yunlzheng <yunl.zheng@gmail.com>
2020-02-24 11:05:06 +0800

    add api to support add version to istio rule

--- pkg/apiserver/controllers/istio.go
+++ pkg/apiserver/controllers/istio.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/alibaba/kt-connect/pkg/apiserver/common"
 	"github.com/gin-gonic/gin"
+	v1alpha3 "istio.io/api/networking/v1alpha3"
 	versionedclient "istio.io/client-go/pkg/clientset/versioned"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -104,4 +105,60 @@ func (c *IstioController) DestinationRule(context *gin.Context) {
 	}
 
 	context.JSON(200, destinationrule)
+}
+
+func (c *IstioController) AddVersionToDestinationRule(context *gin.Context) {
+	namespace := context.Param("namespace")
+	name := context.Param("name")
+	version := context.Param("version")
+
+	ic, err := versionedclient.NewForConfig(c.Context.Cluster.Config)
+
+	if err != nil {
+		context.JSON(500, gin.H{
+			"message": "fail init istioClient",
+		})
+		return
+	}
+
+	destinationrule, err := ic.NetworkingV1alpha3().DestinationRules(namespace).Get(name, metav1.GetOptions{})
+	if err != nil {
+		context.JSON(500, gin.H{
+			"message": "fail get destinationrule",
+		})
+		return
+	}
+
+	for _, subset := range destinationrule.Spec.Subsets {
+		if subset.Name == version {
+			context.JSON(422, gin.H{
+				"message": "version already present",
+			})
+			return
+		}
+	}
+
+	newSubset := &v1alpha3.Subset{
+		Name: version,
+		Labels: map[string]string{
+			version: version,
+		},
+	}
+	destinationrule.Spec.Subsets = append(destinationrule.Spec.Subsets, newSubset)
+
+	result, err := ic.NetworkingV1alpha3().DestinationRules(namespace).Update(destinationrule)
+	if err != nil {
+		context.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(200, result)
+}
+
+func (c *IstioController) RemoveVersionToDestinationRule(context *gin.Context) {
+	context.JSON(200, gin.H{
+		"message": "remove version to destinaltion rule",
+	})
 }
