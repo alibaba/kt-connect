e335bf8f58712ff38a65476fde0409c662295605
Zeusro <692486802@qq.com>
2020-03-03 12:42:26 +0800

    improvement : use Informer to waitPodReady

--- pkg/kt/cluster/kubernetes.go
+++ pkg/kt/cluster/kubernetes.go
@@ -1,6 +1,7 @@
 package cluster
 
 import (
+	"errors"
 	"time"
 
 	clusterWatcher "github.com/alibaba/kt-connect/pkg/apiserver/cluster"
@@ -8,9 +9,14 @@ import (
 	"github.com/rs/zerolog/log"
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -77,7 +83,8 @@ func CreateShadow(
 	}
 	log.Info().Msgf("Deploying shadow deployment %s in namespace %s\n", result.GetObjectMeta().GetName(), namespace)
 
-	pod, err := waitPodReady(namespace, name, clientset)
+	// pod, err := waitPodReady(namespace, name, clientset)
+	pod, err := waitPodReadyUsingInformer(namespace, name, clientset)
 	if err != nil {
 		return
 	}
@@ -87,6 +94,7 @@ func CreateShadow(
 	return
 }
 
+// Deprecated : this implement is not "golang style"
 func waitPodReady(namespace, name string, clientset *kubernetes.Clientset) (pod apiv1.Pod, err error) {
 	pod = apiv1.Pod{}
 	for {
@@ -112,6 +120,89 @@ func waitPodReady(namespace, name string, clientset *kubernetes.Clientset) (pod 
 	}
 	log.Info().Msg("Shadow is ready.")
 	return
+}
+
+func waitPodReadyUsingInformer(namespace, name string, clientset *kubernetes.Clientset) (pod apiv1.Pod, err error) {
+	pod = apiv1.Pod{}
+	// sharedInformerFactory:=informers.SharedInformerOption()
+	option := informers.WithNamespace(namespace)
+	factory := informers.NewSharedInformerFactoryWithOptions(clientset, 0, option)
+	podInformer := factory.Core().V1().Pods()
+	podLabels := labels.NewSelector()
+	log.Info().Msgf("pod label: kt=%s", name)
+	labelKeys := []string{
+		"kt",
+	}
+	requirement, err := labels.NewRequirement(labelKeys[0], selection.Equals, []string{name})
+	if err != nil {
+		return
+	}
+	podLabels.Add(*requirement)
+	// Kubernetes serves an utility to handle API crashes
+	defer runtime.HandleCrash()
+	stopSignal := make(chan struct{})
+	defer close(stopSignal)
+	go factory.Start(stopSignal)
+	if !cache.WaitForNamedCacheSync("attach detach", stopSignal,
+		podInformer.Informer().HasSynced) {
+		err = errors.New("Error waiting for the informer caches to sync")
+		if err != nil {
+			return pod, err
+		}
+	}
+	pods, err := podInformer.Lister().Pods(namespace).List(podLabels)
+	if err != nil {
+		return pod, err
+	}
+	log.Info().Msg("podInformer init")
+	getTargetPod := func(podList []*v1.Pod) *v1.Pod {
+		// log.Info().Msgf("len(podList):%d", len(podList))
+		for _, p := range podList {
+			if len(p.Labels) <= 0 {
+				// almost impossible
+				continue
+			}
+			item, containKey := p.Labels[labelKeys[0]]
+			if !containKey || item != name {
+				continue
+			}
+			return p
+		}
+		return nil
+	}
+	wait := func(podName string) {
+		time.Sleep(time.Second)
+		if len(podName) >= 0 {
+			log.Info().Msgf("pod: %s is running,but not ready", podName)
+			return
+		}
+		log.Info().Msg("Shadow Pods not ready......")
+	}
+wait_loop:
+	for {
+		hasRunningPod := len(pods) > 0
+		var podName string
+		if hasRunningPod {
+			// podLister do not support FieldSelector
+			// https://github.com/kubernetes/client-go/issues/604
+			p := getTargetPod(pods)
+			if p != nil {
+				if p.Status.Phase == "Running" {
+					pod = *p
+					log.Info().Msgf("Shadow pod: %s is ready.", pod.Name)
+					break wait_loop
+				}
+				podName = p.Name
+			}
+		}
+		wait(podName)
+		pods, err = podInformer.Lister().Pods(namespace).List(podLabels)
+		if err != nil {
+			return pod, err
+		}
+	}
+	<-stopSignal
+	return pod, nil
 }
 
 func generatorDeployment(namespace, name string, labels map[string]string, image string) *appsv1.Deployment {
