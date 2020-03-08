22c40a6f72a2a8f736c59a8e415c4db7a2208885
yunlzheng <yunl.zheng@gmail.com>
2020-03-08 13:33:17 +0800

    fix: fixed GetRandomSSHPort fail and add test case.

--- pkg/kt/util/network.go
+++ pkg/kt/util/network.go
@@ -3,13 +3,25 @@ package util
 import (
 	"fmt"
 	"net"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 )
 
 // GetRandomSSHPort get pod random ssh port
 func GetRandomSSHPort(podIP string) string {
-	return fmt.Sprintf("22%s", podIP[len(podIP)-2:])
+	parts := strings.Split(podIP, ".")
+	rdm := parts[len(parts)-1]
+
+	if len(rdm) == 1 {
+		rdm = fmt.Sprintf("0%s", rdm)
+	}
+
+	if len(rdm) > 2 {
+		rdm = rdm[len(rdm)-2:]
+	}
+
+	return fmt.Sprintf("22%s", rdm)
 }
 
 // GetOutboundIP Get preferred outbound ip of this machine
