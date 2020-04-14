0913ac14e913cc97abe8d81be21dded32d4e3e4e
yunlzheng <yunl.zheng@gmail.com>
2020-04-14 13:14:18 +0800

    feat: #116 adator kubectl commands

--- pkg/kt/exec/kubectl/commands.go
+++ pkg/kt/exec/kubectl/commands.go
@@ -5,37 +5,66 @@ import (
 	"os/exec"
 )
 
+// KUBECTL the path to kubectl
+var KUBECTL = "kubectl"
+
 // Version ...
 func (k *Cli) Version() *exec.Cmd {
-	return exec.Command("kubectl", "--kubeconfig="+k.KubeConfig, "version", "--short", "port-forward")
+	args := kubectl(k, "")
+	args = append(args, "version", "--short", "port-forward")
+	return exec.Command(KUBECTL,
+		args...,
+	)
 }
 
 // ApplyDashboardToCluster ...
 func (k *Cli) ApplyDashboardToCluster() *exec.Cmd {
-	return exec.Command(
-		"kubectl",
-		"--kubeconfig="+k.KubeConfig,
-		"-n",
-		"kube-system",
+	args := kubectl(k, "kube-system")
+	args = append(
+		args,
 		"apply",
 		"-f",
 		"https://raw.githubusercontent.com/alibaba/kt-connect/master/docs/deploy/manifest/all-in-one.yaml")
+	return exec.Command(
+		KUBECTL,
+		args...)
 }
 
 // PortForwardDashboardToLocal ...
 func (k *Cli) PortForwardDashboardToLocal(port string) *exec.Cmd {
-	return exec.Command(
-		"kubectl",
-		"--kubeconfig="+k.KubeConfig,
-		"-n",
-		"kube-system",
-		"port-forward",
+	args := kubectl(k, "kube-system")
+	args = append(args, "port-forward",
 		"service/kt-dashboard",
-		port+":80",
+		port+":80")
+	return exec.Command(
+		KUBECTL,
+		args...,
 	)
 }
 
 // PortForward ...
 func (k *Cli) PortForward(namespace, resource string, remotePort int) *exec.Cmd {
-	return exec.Command("kubectl", "--kubeconfig="+k.KubeConfig, "-n", namespace, "port-forward", resource, fmt.Sprintf("%d", remotePort)+":22")
+	args := kubectl(k, namespace)
+	args = append(args, "port-forward",
+		resource,
+		fmt.Sprintf("%d", remotePort)+":22")
+	return exec.Command(
+		KUBECTL,
+		args...,
+	)
+}
+
+func kubectl(k *Cli, namespace string) []string {
+	var args []string
+
+	if k.KubeConfig != "" {
+		args = append(args, "--kubeconfig="+k.KubeConfig)
+	}
+
+	if namespace != "" {
+		args = append(args, "-n")
+		args = append(args, namespace)
+	}
+
+	return args
 }
