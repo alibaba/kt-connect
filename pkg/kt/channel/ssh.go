f7460f56bafd2fcd4f55bf7b04eaba6044456653
yunlzheng <yunl.zheng@gmail.com>
2020-09-08 14:14:21 +0800

    refactor: clean code.

--- pkg/kt/channel/ssh.go
+++ pkg/kt/channel/ssh.go
@@ -17,17 +17,7 @@ type SSHChannel struct{}
 
 // StartSocks5Proxy start socks5 proxy
 func (c *SSHChannel) StartSocks5Proxy(certificate *Certificate, sshAddress string, socks5Address string) (err error) {
-	return DynamicPortForward(certificate.Username, certificate.Password, sshAddress, socks5Address)
-}
-
-// ForwardRemoteToLocal forward remote request to local
-func (c *SSHChannel) ForwardRemoteToLocal(certificate *Certificate, sshAddress string, remoteEndpoint string, localEndpoint string) (err error) {
-	return ForwardRemoteToLocal(certificate.Username, certificate.Password, sshAddress, remoteEndpoint, localEndpoint)
-}
-
-// DynamicPortForward create socks5 proxy
-func DynamicPortForward(username string, password string, address string, socks5Address string) error {
-	conn, err := connection(username, password, address)
+	conn, err := connection(certificate.Username, certificate.Password, sshAddress)
 	defer conn.Close()
 	if err != nil {
 		return err
@@ -54,8 +44,8 @@ func DynamicPortForward(username string, password string, address string, socks5
 }
 
 // ForwardRemoteToLocal forward remote request to local
-func ForwardRemoteToLocal(username string, password string, address string, remoteEndpoint string, localEndpoint string) (err error) {
-	conn, err := connection(username, password, address)
+func (c *SSHChannel) ForwardRemoteToLocal(certificate *Certificate, sshAddress string, remoteEndpoint string, localEndpoint string) (err error) {
+	conn, err := connection(certificate.Username, certificate.Password, sshAddress)
 	defer conn.Close()
 	if err != nil {
 		log.Error().Msgf("fail to create ssh tunnel")
