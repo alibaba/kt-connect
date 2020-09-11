81077e78798fe3edd2fb5b4a626d01dbe0dcac8f
yunlzheng <yunl.zheng@gmail.com>
2020-09-11 15:30:56 +0800

    fix: default image version

--- pkg/kt/command/flags.go
+++ pkg/kt/command/flags.go
@@ -25,7 +25,7 @@ func AppFlags(options *options.DaemonOptions, version string) []cli.Flag {
 		cli.StringFlag{
 			Name:        "image,i",
 			Usage:       "Custom proxy image",
-			Value:       "registry.cn-hangzhou.aliyuncs.com/rdc-incubator/kt-connect-shadow:" + version,
+			Value:       "registry.cn-hangzhou.aliyuncs.com/rdc-incubator/kt-connect-shadow:v" + version,
 			Destination: &options.Image,
 		},
 		cli.BoolFlag{
