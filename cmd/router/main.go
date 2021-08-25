e621b154fd75c5b5a5d6a0a30ac16c49429562e9
金戟 <jinji.lf@alibaba-inc.com>
2021-08-25 23:56:01 +0800

    feat: send sighup to nginx

--- cmd/router/main.go
+++ cmd/router/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"os"
+	"syscall"
 	"text/template"
 
 	"github.com/rs/zerolog"
@@ -17,10 +18,14 @@ func init() {
 //go:embed route.conf
 var routeTemplate string
 
+const pathKtLock = "/var/kt.lock"
+const pathKtConf = "/etc/kt.conf"
+const pathRouteConf = "/etc/nginx/conf.d/route.conf"
+
 func main() {
 	tmpl, err := template.New("route").Parse(routeTemplate)
 	if err != nil {
-		log.Error().Msgf("Failed to load route template")
+		log.Error().Msgf("Failed to load route template: %s", err)
 		return
 	}
 
@@ -30,8 +35,29 @@ func main() {
 		"Versions": []string{"v2", "v3", "v4"},
 	}
 
-	err = tmpl.Execute(os.Stdout, data)
+	routeConf, err := os.Create(pathRouteConf)
 	if err != nil {
-		log.Error().Msgf("Failed to generate route configuration")
+		log.Error().Msgf("Failed to create route configuration file: %s", err)
+		return
 	}
+	defer routeConf.Close()
+
+	err = tmpl.Execute(routeConf, data)
+	if err != nil {
+		log.Error().Msgf("Failed to generate route configuration: %s", err)
+		return
+	}
+
+	process, err := os.FindProcess(1)
+	if err != nil {
+		log.Error().Msgf("Failed to find route process: %s", err)
+		return
+	}
+	err = process.Signal(syscall.SIGHUP)
+	if err != nil {
+		log.Error().Msgf("Failed to reload route configuration: %s", err)
+	}
+
+	log.Info().Msgf("Route updated.")
+
 }
