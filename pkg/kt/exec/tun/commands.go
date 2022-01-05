d91db4a3ff40ab8eefb51d4070a6e9d7f4c45da2
金戟 <jinji.lf@alibaba-inc.com>
2022-01-05 14:08:11 +0800

    feat: turn off tun2sock debug log

--- pkg/kt/exec/tun/commands.go
+++ pkg/kt/exec/tun/commands.go
@@ -9,13 +9,17 @@ import (
 )
 
 // ToSocks create a tun and connect to socks endpoint
-func (s *Cli) ToSocks(sockAddr string) error {
+func (s *Cli) ToSocks(sockAddr string, isDebug bool) error {
 	tunSignal := make(chan error)
+	logLevel := "warning"
+	if isDebug {
+		logLevel = "debug"
+	}
 	go func() {
 		var key = new(engine.Key)
 		key.Proxy = sockAddr
 		key.Device = s.getTunName()
-		key.LogLevel = "debug"
+		key.LogLevel = logLevel
 		engine.Insert(key)
 		tunSignal <-engine.Start()
 
