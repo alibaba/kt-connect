a778fa2c65ab49faaff9c73695b77d1a7c3c5356
金戟 <jinji.lf@alibaba-inc.com>
2022-02-03 20:43:18 +0800

    feat: more logs during shadow start

--- build/docker/shadow/run.sh
+++ build/docker/shadow/run.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-mkdir -p /root/.ssh
 
-# mounted from config map
-cp /root/authorized/authorized_keys /root/.ssh
+echo "shadow starting ..."
+
+# fetch authorized_keys from volume mounted via config map
+mkdir -p /root/.ssh
+cp /root/authorized/authorized_keys /root/.ssh/authorized_keys
 
 if [ -n "${privateKey}" ]; then
   # for ephemeral container
   # private key and authorized_keys must be base64 encoded in environment
-  echo "${privateKey}" |base64 -d > /root/.ssh/id_rsa
+  echo "${privateKey}" | base64 -d > /root/.ssh/id_rsa
+  echo "private key created created"
 fi
 
 /usr/sbin/sshd -D &
+echo "sshd triggered"
+
+sleep 1
+ps -ef | grep -v 'ps -ef' | cat
 
 if [[ "${1}" = "--debug" ]]; then
-   /usr/sbin/dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient exec /usr/sbin/shadow
+  echo "run in debug mode"
+  /usr/sbin/dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient exec /usr/sbin/shadow
 else
-   /usr/sbin/shadow
+  echo "run in standard mode"
+  /usr/sbin/shadow
 fi
